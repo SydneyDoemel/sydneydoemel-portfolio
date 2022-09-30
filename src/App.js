84d615038dec090698d3ed5c06cdd1992eb15f35
Sydney C Doemel Doemel <sydneycdoemeldoemel@sydneys-mbp.attlocal.net>
2022-09-30 00:38:35 -0400

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter, Router } from 'react-router-dom'
+import Home from './views/Home';
+import About from './views/About';
+import Portfolio from './views/Portfolio';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     
+    <Nav />
+    <Routes>
+    <Route path='/home' element={<Home />}/>
+    <Route path='/about' element={<About />}/>
+    <Route path='/portfolio' element={<Portfolio />}/>
+    </Routes>
+      
     </div>
+    </BrowserRouter>
   );
 }
 
